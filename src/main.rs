a7a9182d4689f94d58355762694148b4ecf77316
Likend <lyk2005@foxmail.com>
2026-04-27 16:30:32 +0800

    Test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use axum::{Router, routing::get};
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, Rust!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener   = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_fail() {
+        assert!(false);
+    }
 }
